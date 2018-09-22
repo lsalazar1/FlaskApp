c8de6c49d24d36d67a19a348036b02cf18dab59e
Liam Salazar <lmsalazar78011@gmail.com>
2018-09-21 19:47:55 -0500

    Comments added

--- app.py
+++ app.py
@@ -5,18 +5,23 @@ app = Flask(__name__)
 
 Homework = Homework()
 
+#routes to homepage
 @app.route('/')
 def index():
     return render_template('home.html')
 
+#routes to about page
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+#routes to homework page
+#CRUD operations used here later
 @app.route('/homework')
 def homework():
     return render_template('homework.html', homework = Homework)
 
+#routes to assignment's specific webpage
 @app.route('/assignment/<string:id>/')
 def assignment(id):
     return render_template('assignment.html', id=id)
