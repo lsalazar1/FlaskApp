1844bdb78a2665378e7e09e53f0ef59496b47fbf
Rommel Salazar <salazro@win.eng.vzwnet.com>
2018-09-21 20:37:55 -0500

    RegistrationForm class and web page created

--- app.py
+++ app.py
@@ -1,31 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
 from data import Homework
+from flask_mysqldb import MYSQL
+from wtforms import Form, StringField, TextAreaField, PasswordField, validators
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 
 Homework = Homework()
 
-#routes to homepage
 @app.route('/')
 def index():
     return render_template('home.html')
 
-#routes to about page
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-#routes to homework page
-#CRUD operations used here later
 @app.route('/homework')
 def homework():
     return render_template('homework.html', homework = Homework)
 
-#routes to assignment's specific webpage
 @app.route('/assignment/<string:id>/')
 def assignment(id):
     return render_template('assignment.html', id=id)
 
+#Registration page
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    form = RegistrationForm(request.form)
+
+    #checks if request method is post or get
+    if request.method == 'POST' and form.validate():
+
+    return render_template('register.html', form = form)
+
+class RegistrationForm(Form):
+    name = StringField('Name', [validators.length(min = 1, max = 50)])
+    username = StringField('Username', [validators.length(min = 2, max = 30)])
+    email = StringField('Email', [validators.length(min=3, max=50)])
+    password = StringField('Password', [
+                    validators.DataRequired(),
+                    validators.EqualTo('confirm', message = 'Passwords do not match')
+    ])
+    confirm = PasswordField('Cofirm Password')
+        
 
 if __name__ == '__main__' :
     app.run(debug=True)
