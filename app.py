bdf00cd021c14967b0417f2be9ad823bd0b335c6
Liam Salazar <lmsalazar78011@gmail.com>
2018-10-14 02:47:55 -0500

    Access control added for security

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from data import Homework
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -18,18 +19,23 @@ mysql = MySQL(app)
 
 Homework = Homework()
 
+# Home page
 @app.route('/')
 def index():
     return render_template('home.html')
 
+# About page
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+# Homework logger
 @app.route('/homework')
 def homework():
     return render_template('homework.html', homework = Homework)
 
+
+# Specific Assignments
 @app.route('/assignment/<string:id>/')
 def assignment(id):
     return render_template('assignment.html', id=id)
@@ -84,16 +90,50 @@ def login():
             password = data['password']
 
             if sha256_crypt.verify(password_candidate, password):
-                app.logger.info('PASSWORD MATCHED')
+                #Successful login
+                session['logged_in'] = True
+                session['username'] = username
+
+                flash('Hello there!', 'success')
+                return redirect(url_for('dashboard'))
             else:
                 error = 'Incorrect Password'
-                return render_template('home.html', error=error)
+                return render_template('login.html', error=error)
+            # Close MySQL connection
+            cur.close()
+
         else:
             error = "No users with that username found"
-            return render_template('home.html', error=error)
+            return render_template('login.html', error=error)
     
     return render_template('login.html')
 
+# Authenticates if user is logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash("You don't have access yet! Please login!" , 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+# Dashboard
+@app.route('/dashboard')
+@is_logged_in
+def dashboard():
+    return render_template('dashboard.html')
+    
+# Logout
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You are now logged out', 'success')
+
+    return redirect(url_for('login'))
+
+# Register Form Class
 class RegistrationForm(Form):
     name = StringField('Name', [validators.length(min = 1, max = 50)])
     username = StringField('Username', [validators.length(min = 2, max = 30)])
